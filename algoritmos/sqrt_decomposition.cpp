806fe958f5babb8178165f100cdf8f3eadf082d7
Uthant Vicentin <uthantvicentin@gmail.com>
2019-08-16 21:21:53 -0400

    SQRT DECOMPOSITION

--- algoritmos/sqrt_decomposition.cpp
+++ algoritmos/sqrt_decomposition.cpp
@@ -1,79 +1,65 @@
-// C++ program to demonstrate working of Square Root 
-// Decomposition. 
-#include "iostream" 
-#include "math.h" 
+#include <bits/stdc++.h>
+
 using namespace std; 
 
 #define MAXN 10000 
 #define SQRSIZE 100 
 
-int arr[MAXN];			 // original array 
-int block[SQRSIZE];		 // decomposed array 
-int blk_sz;					 // block size 
+int arr[MAXN];			 // vetor original 
+int block[SQRSIZE];		 // vetor decomposto 
+int blk_sz;					 // tamanho do bloco
 
-// Time Complexity : O(1) 
-void update(int idx, int val) 
-{ 
+// Complexidade : O(1) 
+void update(int idx, int val){ 
 	int blockNumber = idx / blk_sz; 
 	block[blockNumber] += val - arr[idx]; 
 	arr[idx] = val; 
 } 
 
-// Time Complexity : O(sqrt(n)) 
-int query(int l, int r) 
-{ 
+// Complexidade : O(sqrt(n)) 
+int query(int l, int r){ 
 	int sum = 0; 
-	while (l<r and l%blk_sz!=0 and l!=0) 
-	{ 
-		// traversing first block in range 
+	while (l<r and l%blk_sz!=0 and l!=0){ 
+		// Caminhando pelo limite esquerdo 
 		sum += arr[l]; 
 		l++; 
 	} 
-	while (l+blk_sz <= r) 
-	{ 
-		// traversing completely overlapped blocks in range 
+	while (l+blk_sz <= r){ 
+		// Caminhando pelos blocos menores do que o limite direito 
 		sum += block[l/blk_sz]; 
 		l += blk_sz; 
 	} 
-	while (l<=r) 
-	{ 
-		// traversing last block in range 
+	while (l<=r){ 
+		// Caminhando pelo bloco do limite direito
 		sum += arr[l]; 
 		l++; 
 	} 
 	return sum; 
 } 
 
-// Fills values in input[] 
-void preprocess(int input[], int n) 
-{ 
-	// initiating block pointer 
+// Preenche os valores na variavel input 
+void preprocess(int input[], int n){ 
+	// Iniciando o index do bloco
 	int blk_idx = -1; 
 
-	// calculating size of block 
+	// Calculo do tamanho do bloco 
 	blk_sz = sqrt(n); 
 
-	// building the decomposed array 
-	for (int i=0; i<n; i++) 
-	{ 
+	// Criando o vetor decomposto 
+	for (int i=0; i<n; i++){ 
 		arr[i] = input[i]; 
-		if (i%blk_sz == 0) 
-		{ 
-			// entering next block 
-			// incementing block pointer 
+		if (i%blk_sz == 0){ 
+			// Entrando no proximo bloco 
+			// Incrementando o valor do indice 
 			blk_idx++; 
 		} 
 		block[blk_idx] += arr[i]; 
 	} 
 } 
 
-// Driver code 
-int main() 
-{ 
-	// We have used separate array for input because 
-	// the purpose of this code is to explain SQRT 
-	// decomposition in competitive programming where 
-	// we have multiple inputs. 
+
+int main(){ 
+	
 	int input[] = {1, 5, 2, 4, 6, 1, 3, 5, 7, 10}; 
 	int n = sizeof(input)/sizeof(input[0]); 
 
